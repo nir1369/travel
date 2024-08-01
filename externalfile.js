2d2380bf8b17ee73260625f6d2ca8894b281b54d
nir1369 <niroulapratyush54@gmail.com>
2024-08-01 16:20:54 +0545

    git push

---
+++ externalfile.js
@@ -0,0 +1,12 @@
+console.log("hello world");
+console.log(20);
+console.log(3.14);
+
+var fullname='ram thapa';
+let ward=20;
+
+console.log(fullname);
+console.log(ward);
+
+fullname='sita chaudary';
+ward='30';
---
+++ style.css-1.m
@@ -0,0 +1,44 @@
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+.grid-container {
+  display: grid;
+  grid-template-columns: 80px 200px auto 30px;
+  gap: 10px;
+  background-color: #2196F3;
+  padding: 10px;
+}
+#
+#meta {
+  padding-top: 220px;
+  padding-bottom: 220px;
+}
+
+meta h1 {
+
+color:#514d59;
+  text-align: center;
+  font-size: 70px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 60px; 
+  margin-bottom: 90px;
+}
+
+#meta small {
+  padding-top: 200px;
+  padding-bottom: 100px;
+}
+.meta-block p {
+  color: #514d59;
+  text-align: center;
+  font-family: Roboto;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 22px; 
+}
+
+
